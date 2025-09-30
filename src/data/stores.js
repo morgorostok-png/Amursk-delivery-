9ea6098745e24b9ed3cf1edcf8659c9fc2efa593
morgorostok-png <morgorostok@gmail.com>
2025-10-01 00:03:36 +1000

    Create stores.js

---
+++ src/data/stores.js
@@ -0,0 +1,26 @@
+export const stores = [
+  {
+    id: 1,
+    name: "Пицца Амурск",
+    menu: [
+      { id: 1, name: "Маргарита", price: 500 },
+      { id: 2, name: "Пепперони", price: 600 }
+    ]
+  },
+  {
+    id: 2,
+    name: "Суши Амурск",
+    menu: [
+      { id: 3, name: "Филадельфия", price: 700 },
+      { id: 4, name: "Калифорния", price: 650 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Продукты Амурск",
+    menu: [
+      { id: 5, name: "Хлеб", price: 40 },
+      { id: 6, name: "Молоко", price: 80 }
+    ]
+  }
+];
